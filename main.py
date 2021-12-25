6b1ec35aede6e2b3d48b73bea3d8ee4eb28c40d1
Akilesh-programmer <akileshakileshs1234@gmail.com>
2021-12-25 11:22:13 +0530

    Updated the code such that after three second english meaning would be shown for the before shown French word. Then we can click right or wrong and the process continues.

--- main.py
+++ main.py
@@ -6,20 +6,58 @@ BACKGROUND_COLOR = "#B1DDC6"
 # With these below two variables only we are going to update the card texts.
 word = "Word"
 language = "French"
+# --------------------------------------------------------Back End-----------------------------------------------------
+# Opening the data file with the help of pandas.
+with open("french_words.csv") as file:
+    data = pandas.read_csv(file)
+    # Converting the data into a dictionary. Without the orient set to records, first we will have all the 100 French
+    # words stored in a dictionary and then well will have another dictionary of English words. But if we use this
+    # function then we can create 100 dictionaries. IN each of them a French word and its English meaning would be
+    # present.
+    data = data.to_dict(orient="records")
+    print(data)
 
 
 # ---------------------------------------------------Functions-----------------------------------------------------------
-# This is a function to generate a random french card. It will get activated when we click on right or wrong button.
+# In this function only we show French word, we give 3 seconds time then we show the english word. Then we wait for the
+# click on right or wrong button then the process continues.
 def generate_random_word():
-    global card_word
-    global card_title
     global word
     global language
+    global card_title
+    global card_word
+    global card_back_img
+    global card_front_img
+
+    # Creating a function to show the English word.
+    def english():
+        global word
+        global language
+        global card_title
+        global card_word
+        english_word = data[random_number]["English"]
+        word = english_word
+        language = "English"
+        # We are deleting the previously created text because then we will be overlapping. Also, if we don't create it
+        # here it will not get updated.
+        canvas.delete(card_word)
+        canvas.delete(card_title)
+        canvas.itemconfig(canvas_image, image=card_back_img)
+        card_word = canvas.create_text(400, 263, text=word, fill="white", font="Arial 30 bold")
+        card_title = canvas.create_text(400, 150, text=language, fill="white", font="Arial 20 italic")
+
     random_number = random.randint(0, 100)
+    language = "French"
     french_word = data[random_number]["French"]
     word = french_word
     canvas.delete(card_word)
-    card_word = card_word = canvas.create_text(400, 263, text=word, font="Arial 30 bold")
+    canvas.delete(card_title)
+    canvas.itemconfig(canvas_image, image=card_front_img)
+    card_word = canvas.create_text(400, 263, text=word, fill="black", font="Arial 30 bold")
+    card_title = canvas.create_text(400, 150, text=language, fill="black", font="Arial 20 italic")
+
+    # Giving three second time with this function. Then we are calling the English function to show the english word.
+    window.after(3000, english)
 
 
 # -------------------------------------------------User Interface ------------------------------------------------------
@@ -31,7 +69,8 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 # Canvas. Using it to put an image on to the screen.
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="card_back.png")
+canvas_image = canvas.create_image(400, 263, image=card_front_img)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 # Text in canvas
 card_title = canvas.create_text(400, 150, text=language, font="Arial 20 italic")
@@ -47,18 +86,6 @@ correct_button.grid(row=1, column=0)
 wrong_image = PhotoImage(file="wrong.png")
 wrong_button = Button(image=wrong_image, highlightthickness=0, command=generate_random_word)
 wrong_button.grid(row=1, column=1)
-
-
-# --------------------------------------------------------Back End-----------------------------------------------------
-# Opening the data file with the help of pandas.
-with open("french_words.csv") as file:
-    data = pandas.read_csv(file)
-    # Converting the data into a dictionary. Without the orient set to records, first we will have all the 100 French
-    # words stored in a dictionary and then well will have another dictionary of English words. But if we use this
-    # function then we can create 100 dictionaries. IN each of them a French word and its English meaning would be
-    # present.
-    data = data.to_dict(orient="records")
-    print(data)
 
 # If we don't call the function here then while we are starting the program, just the words, "Title" and "Word" will be
 # only displayed because it is the starting.
